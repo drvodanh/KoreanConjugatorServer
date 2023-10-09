b4c281e786b1fd80673737bf07e7d9f185ffd2ce
drvodanh <80259976+drvodanh@users.noreply.github.com>
2023-10-09 03:28:51 +0000

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ let database = require('./database');
 const express = require('express');
 const app = express();
 
-const server_port = 8080;
-const server_ip_address = '127.0.0.1';
+#const server_port = 8080;
+#const server_ip_address = '127.0.0.1';
 const CONJUGATE_ROUTE = '/conjugate=';
 
 app.get('/', function(req, res){
