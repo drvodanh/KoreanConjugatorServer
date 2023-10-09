26229a0e1d7b9a6a66ae978e8c3533d5538d5982
drvodanh <80259976+drvodanh@users.noreply.github.com>
2023-10-09 03:49:45 +0000

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 const server_port = process.env.PORT || 3000
-const server_ip_address = process.env.CYCLIC_URL || 'http://localhost';
+const server_ip_address = 'http://localhost';
 const CONJUGATE_ROUTE = '/conjugate=';
 
 app.get('/', function(req, res){
