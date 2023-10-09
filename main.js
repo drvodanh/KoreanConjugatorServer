bb9f842abfab330b308c4c0c00e8702ddb8b0ad7
drvodanh <80259976+drvodanh@users.noreply.github.com>
2023-10-09 06:59:24 +0000

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 const server_port = 3000;
-//const server_ip_address = "localhost";
+const server_ip_address = "localhost";
 const CONJUGATE_ROUTE = '/conjugate=';
 
 app.get('/', function(req, res){
